d3f024838bc357dd70ff9e4bd2e3f54f5f06e2b4
Marco Carvalho <marcolucio27@gmail.com>
2019-04-21 04:32:20 -0300

    update template

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,52 @@
 <template lang="pug">
 .container
-  h1.text-center Calculadora de Juros Compostos
-  .form-group
-    label.font-weight-bold Valor Inicial:
-    input.form-control(v-model.number="valorInicial" type="number" placeholder="ex: 15000")
-  .row.form-group
-    .col-6
-      label.font-weight-bold Aporte Mensal:
-      input.form-control(v-model.number="aporteMensal" type="number" placeholder="ex: 2200")
-    .col
-      label.font-weight-bold Aporte Anual:
-      input.form-control(v-model.number="aporteAnual" type="number" placeholder="ex: 100000")
-    .col
-      label.font-weight-bold Mês do Aporte:
+  .h1.text-center.text-uppercase Calculadora de Juros Compostos
+  hr
+  .mb-3
+    label.font-weight-bold Valor Inicial (em R$):
+    input.form-control(v-model.number="valorInicial" type="number" placeholder="ex: 15000" min="1")
+  .row
+    .col-lg-8.mb-3
+      label.font-weight-bold Aporte Mensal (em R$):
+      input.form-control(v-model.number="aporteMensal" type="number" placeholder="ex: 2200" min="1")
+    .col-lg-4.mb-3
+      label.font-weight-bold Correção Anual do Aporte Mensal (em %):
+      input.form-control(v-model.number="correcaoAporteMensal" type="number" placeholder="ex: 10" min="1")
+  .row
+    .col-lg-4.mb-3
+      label.font-weight-bold Aporte Anual (em R$):
+      input.form-control(v-model.number="aporteAnual" type="number" placeholder="ex: 100000" min="1")
+    .col-lg-4.mb-3
+      label.font-weight-bold Mês do Aporte Anual:
       input.form-control(v-model.number="mesAporteAnual" type="number" min="1" max="12")
-  .form-group
-    label.font-weight-bold Taxa Mensal (em %):
-    input.form-control(v-model.number="taxaMensal" type="number" placeholder="ex: 0.5")
-  .form-group
-    label.font-weight-bold Prazo (em meses):
-    input.form-control(v-model.number="prazo" type="number" placeholder="ex: 108" min="1")
+    .col-lg-4.mb-3
+      label.font-weight-bold Correção Anual do Aporte Anual (em %):
+      input.form-control(v-model.number="correcaoAporteAnual" type="number" placeholder="ex: 10" min="1" disabled)
+  .row
+    .col-lg.mb-3
+      label.font-weight-bold Rendimento (mensal em %):
+      input.form-control(v-model.number="rendimento" type="number" placeholder="ex: 0.5" min="1")
+    .col-lg.mb-3
+      label.font-weight-bold Rendimento (anual em %):
+      input.form-control(@keyup="rendimento = ($event.target.value/12)" type="number" placeholder="ex: 6" min="1")
+  .row
+    .col-lg.mb-3
+      label.font-weight-bold Prazo (em meses):
+      input.form-control(v-model.number="prazo" type="number" placeholder="ex: 108" min="1")
+    .col-lg.mb-3
+      label.font-weight-bold Prazo (em anos):
+      input.form-control(@keyup="prazo = ($event.target.value*12).toFixed(0)" type="number" placeholder="ex: 9" min="1")
   .table-responsive
-    table.table.table-sm.table-hover
+    table.table.table-sm.table-hover.table-striped
       thead
         th.text-center Mês
-        th.text-center Saldo Parcial
-        th.text-center Aporte Mensal
-        th.text-center Aporte Anual
-        th.text-center Acumulado
+        th.text-right Saldo Parcial
+        th.text-right Aporte Mensal
+        th.text-right Aporte Anual
+        th.text-right Acumulado
       tbody
         tr(v-for="(posicao, index) in getPosicoes()")
-          td.text-center {{posicao.mes}}
+          td.text-center {{$moment(posicao.data).format("MM/YY")}}
           td.text-right {{formatToCurrency(posicao.saldoParcial)}}
           td.text-right {{formatToCurrency(posicao.aporteMensal)}}
           td.text-right {{formatToCurrency(posicao.aporteAnual)}}
@@ -43,9 +59,11 @@ export default {
     return {
       valorInicial: 15000,
       aporteMensal: 2200,
+      correcaoAporteMensal: 10,
       aporteAnual: 50000,
       mesAporteAnual: 2,
-      taxaMensal: 0.5,
+      correcaoAporteAnual: 10,
+      rendimento: 0.5,
       prazo: 2*12,
     }
   },
