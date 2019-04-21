63d322b6b949077546d2c78739517670e97917f2
Marco Carvalho <marcolucio27@gmail.com>
2019-04-21 13:41:32 -0300

    update formatToCurrency method

--- src/App.vue
+++ src/App.vue
@@ -73,6 +73,9 @@ export default {
   },
   methods: {
     formatToCurrency(value) {
+      if(!value) {
+        return;
+      }
       return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
     },
     getPosicoes() {
