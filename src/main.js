26d2ed59fadde5a4aba11e88d69fbc168e2bef50
Marco Carvalho <marcolucio27@gmail.com>
2019-04-21 01:41:49 -0300

    import dependencies

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import moment from 'moment';
+
+Vue.prototype.$moment = moment;
+Vue.prototype.$moment.locale('pt-br');
 Vue.config.productionTip = false
 
 new Vue({
