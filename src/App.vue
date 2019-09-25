239d7832a71053b3c866409183e964e17ce5a58d
Marco Carvalho <marcolucio27@gmail.com>
2019-09-24 21:06:42 -0300

    atualizando funcionalidades do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,95 @@
 <template lang="pug">
 .container.mx-auto
-  .text-center.uppercase.font-bold.font-2xl Calculadora de Juros Compostos
+  .text-center.uppercase.font-bold.text-2xl Calculadora de Juros Compostos
   hr
   .mb-4
-    label.block.font-bold Valor Inicial (em R$):
-    input.h-6.border-b.border-green-500.w-full(
+    InputComponent(
+      label="Valor Inicial (em R$)"
       v-model.number="valorInicial"
       type="number"
       placeholder="ex: 15000"
-      class="focus:outline-none"
     )
   .flex.-mx-2
     .px-2.mb-4(class="w-2/3")
-      label.block.font-bold Aporte Mensal (em R$):
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Aporte Mensal (em R$)"
         v-model.number="aporteMensal"
         type="number"
         placeholder="ex: 2200"
-        class="focus:outline-none"
       )
     .px-2.mb-4(class="w-1/3")
-      label.block.font-bold Correção Anual do Aporte Mensal:
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Correção Anual do Aporte Mensal"
+        v-model.number="correcaoAnualAporteMensal"
         type="number"
         placeholder="ex: 0.1"
-        v-model.number="correcaoAnualAporteMensal"
-        class="focus:outline-none"
       )
   .flex.-mx-2
-    .px-2.mb-4(class="w-1/3")
-      label.block.font-bold Aporte Anual (em R$):
-      input.h-6.border-b.border-green-500.w-full(
+    .px-2.mb-4(class="w-1/4")
+      label.block.font-bold(
+        class="md:w-2/3"
+      )
+        div Aporte Anual?
+        input.mr-2(
+          type="checkbox"
+          v-model="aporteAnualHabilitado"
+        )
+    .px-2.mb-4(class="w-1/4")
+      InputComponent(
+        label="Aporte Anual (em R$)"
+        v-model.number="aporteAnual"
         type="number"
         placeholder="ex: 100000"
-        v-model.number="aporteAnual"
-        class="focus:outline-none"
+        :disabled="!aporteAnualHabilitado"
       )
-    .px-2.mb-4(class="w-1/3")
-      label.block.font-bold Mês do Aporte Anual:
-      select.h-6.border-b.border-green-500.w-full.bg-white(
+    .px-2.mb-4(class="w-1/4")
+      SelectComponent(
+        label="Mês do Aporte Anual"
         v-model.number="mesAporteAnual"
-        class="focus:outline-none"
+        :options="Array.apply(0, Array(12)).map((_,i) => $moment().month(i).format('MMMM'))"
+        :disabled="!aporteAnualHabilitado"
       )
-        option(
-          v-for="(i, index) in Array.apply(0, Array(12)).map((_,i) => $moment().month(i).format('MMMM'))"
-          :value="index"
-        ) {{i}}
-    .px-2.mb-4(class="w-1/3")
-      label.block.font-bold Correção Anual do Aporte Anual:
-      input.h-6.border-b.border-green-500.w-full(
+    .px-2.mb-4(class="w-1/4")
+      InputComponent(
+        label="Correção Anual do Aporte Anual"
+        v-model.number="correcaoAnualAporteAnual"
         type="number"
         placeholder="ex: 0.1"
-        disabled
-        v-model.number="correcaoAnualAporteAnual"
-        class="focus:outline-none"
+        :disabled="!aporteAnualHabilitado"
       )
   .flex.-mx-2
     .px-2.mb-4(class="w-1/2")
-      label.block.font-bold Rendimento (mensal):
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Rendimento (mensal)"
+        v-model.number="rendimentoMensal"
         type="number"
         placeholder="ex: 0.005"
-        v-model.number="rendimentoMensal" @input="rendimentoAnual = Math.pow(1+Number($event.target.value), 12)-1"
-        class="focus:outline-none"
+        @input="rendimentoAnual = Math.pow(1+Number(rendimentoMensal), 12)-1"
       )
     .px-2.mb-4(class="w-1/2")
-      label.block.font-bold Rendimento (anual):
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Rendimento (anual)"
+        v-model.number="rendimentoAnual"
         type="number"
         placeholder="ex: 0.06"
-        v-model.number="rendimentoAnual" @input="rendimentoMensal = Math.pow(1+Number($event.target.value), 1/12)-1"
-        class="focus:outline-none"
+        @input="rendimentoMensal = Math.pow(1+Number(rendimentoAnual), 1/12)-1"
       )
   .flex.-mx-2
     .px-2.mb-4(class="w-1/2")
-      label.block.font-bold Prazo (em meses):
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Prazo (em meses)"
+        v-model.number="prazoMensal"
         type="number"
         placeholder="ex: 108"
-        v-model.number="prazoMensal"
-        @input="prazoAnual = ($event.target.value/12).toFixed(2)"
-        class="focus:outline-none"
+        @input="prazoAnual = (prazoMensal/12).toFixed(2)"
       )
     .px-2.mb-4(class="w-1/2")
-      label.block.font-bold Prazo (em anos):
-      input.h-6.border-b.border-green-500.w-full(
+      InputComponent(
+        label="Prazo (em anos)"
         v-model.number="prazoAnual"
-        @input="prazoMensal = ($event.target.value*12).toFixed(0)"
         type="number"
         placeholder="ex: 9"
-        class="focus:outline-none"
+        @input="prazoMensal = (prazoAnual*12).toFixed(0)"
       )
   button.mb-4.bg-green-500.w-full.rounded.p-1(
     class="focus:outline-none hover:bg-green-600"
@@ -118,6 +118,9 @@
 </template>
 
 <script>
+import InputComponent from "@/components/InputComponent"
+import SelectComponent from "@/components/SelectComponent"
+
 export default {
   data() {
     return {
@@ -130,8 +133,9 @@ export default {
       correcaoAnualAporteAnual: 0.1,
       rendimentoMensal: 0.004867550565343048,
       rendimentoAnual: 0.06,
-      prazoMensal: 2*12,
+      prazoMensal: 24,
       prazoAnual: 2,
+      aporteAnualHabilitado: false,
     }
   },
   methods: {
@@ -150,21 +154,25 @@ export default {
         aporteAnual: 0,
       });
       for(let i = 0; i <= this.prazoMensal; i++) {
-        const {data, saldoParcial, aporteMensal, aporteAnual} = posicoes[i];
         const posicao = {};
+        const {data, saldoParcial, aporteMensal, aporteAnual} = posicoes[i];
         posicao.data = this.$moment(data).add(1, "M").toDate();
         posicao.saldoParcial = (saldoParcial + aporteMensal + aporteAnual) * (1+this.rendimentoMensal);
         const newLocal = this.$moment(data).year() === this.$moment(posicao.data).year()
           ? 1
           : 1 + this.correcaoAnualAporteMensal;
         posicao.aporteMensal = aporteMensal * newLocal;
-        posicao.aporteAnual = this.$moment(posicao.data).month() === this.mesAporteAnual
+        posicao.aporteAnual = this.aporteAnualHabilitado && this.$moment(posicao.data).month() === this.mesAporteAnual
           ? this.aporteAnual
           : 0.00;
         posicoes.push(posicao);
       }
       this.posicoes = posicoes;
     }
+  },
+  components: {
+    InputComponent,
+    SelectComponent,
   }
 }
 </script>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div
+  label.block.font-bold {{label}}:
+  input.h-6.border-b.w-full(
+    :value="value"
+    class="focus:outline-none"
+    :class="disabled ? 'bg-gray-300 border-red-500' : 'border-green-500'"
+    @input="$emit('input', $event.target.value)"
+    :disabled="disabled"
+    :type="type"
+    :placeholder="placeholder"
+  )
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    value: Number,
+    disabled: Boolean,
+    type: String,
+    placeholder: String,
+  }
+}
+</script>
